f7ade31b11e5e183e03e1430a4a6f1b3771278fa
Martin Roa Villescas <m.roa.villescas@tue.nl>
2020-08-02 13:16:17 +0200

    Rename test file

--- test/test_julia_cell.py
+++ test/test_julia_cell.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 
 import unittest
 
-from python import ipython_cell as ic
+from python import julia_cell as jc
 
 
 CELL_BOUNDARIES = [1, 4, 8, 15, 20]
@@ -30,81 +30,81 @@ class Buffer(object):
 class TestJuliaCell(unittest.TestCase):
     def test_get_current_cell_boundaries_cursor_start_of_cell(self):
         current_row = 8
-        cell_start, cell_end = ic._get_current_cell_boundaries(current_row,
+        cell_start, cell_end = jc._get_current_cell_boundaries(current_row,
                                                                CELL_BOUNDARIES)
         self.assertEqual(cell_start, 8)
         self.assertEqual(cell_end, 14)
 
     def test_get_current_cell_boundaries_cursor_middle_of_cell(self):
         current_row = 10
-        cell_start, cell_end = ic._get_current_cell_boundaries(current_row,
+        cell_start, cell_end = jc._get_current_cell_boundaries(current_row,
                                                                CELL_BOUNDARIES)
         self.assertEqual(cell_start, 8)
         self.assertEqual(cell_end, 14)
 
     def test_get_current_cell_boundaries_cursor_end_of_cell(self):
         current_row = 14
-        cell_start, cell_end = ic._get_current_cell_boundaries(current_row,
+        cell_start, cell_end = jc._get_current_cell_boundaries(current_row,
                                                                CELL_BOUNDARIES)
         self.assertEqual(cell_start, 8)
         self.assertEqual(cell_end, 14)
 
     def test_get_current_cell_boundaries_cursor_first_cell(self):
         current_row = 2
-        cell_start, cell_end = ic._get_current_cell_boundaries(current_row,
+        cell_start, cell_end = jc._get_current_cell_boundaries(current_row,
                                                                CELL_BOUNDARIES)
         self.assertEqual(cell_start, 1)
         self.assertEqual(cell_end, 3)
 
     def test_get_current_cell_boundaries_cursor_last_cell(self):
         current_row = 20
-        cell_start, cell_end = ic._get_current_cell_boundaries(current_row,
+        cell_start, cell_end = jc._get_current_cell_boundaries(current_row,
                                                                CELL_BOUNDARIES)
         self.assertEqual(cell_start, 20)
         self.assertEqual(cell_end, None)
 
     def test_get_current_cell_boundaries_cursor_start_of_file(self):
         current_row = 1
-        cell_start, cell_end = ic._get_current_cell_boundaries(current_row,
+        cell_start, cell_end = jc._get_current_cell_boundaries(current_row,
                                                                CELL_BOUNDARIES)
         self.assertEqual(cell_start, 1)
         self.assertEqual(cell_end, 3)
 
     def test_get_current_cell_boundaries_cursor_end_of_file(self):
         current_row = 99
-        cell_start, cell_end = ic._get_current_cell_boundaries(current_row,
+        cell_start, cell_end = jc._get_current_cell_boundaries(current_row,
                                                                CELL_BOUNDARIES)
         self.assertEqual(cell_start, 20)
         self.assertEqual(cell_end, None)
 
     def test_get_next_cell_cursor_start_of_cell(self):
         current_row = 8
-        next_cell = ic._get_next_cell(current_row, CELL_BOUNDARIES)
+        next_cell = jc._get_next_cell(current_row, CELL_BOUNDARIES)
         self.assertEqual(next_cell, 15)
 
     def test_get_next_cell_cursor_middle_of_cell(self):
         current_row = 12
-        next_cell = ic._get_next_cell(current_row, CELL_BOUNDARIES)
+        next_cell = jc._get_next_cell(current_row, CELL_BOUNDARIES)
         self.assertEqual(next_cell, 15)
 
     def test_get_next_cell_cursor_end_of_cell(self):
         current_row = 14
-        next_cell = ic._get_next_cell(current_row, CELL_BOUNDARIES)
+        next_cell = jc._get_next_cell(current_row, CELL_BOUNDARIES)
         self.assertEqual(next_cell, 15)
 
     def test_get_prev_cell_cursor_start_of_cell(self):
         current_row = 8
-        prev_cell = ic._get_prev_cell(current_row, CELL_BOUNDARIES)
+        prev_cell = jc._get_prev_cell(current_row, CELL_BOUNDARIES)
         self.assertEqual(prev_cell, 4)
 
     def test_get_prev_cell_cursor_middle_of_cell(self):
         current_row = 10
-        prev_cell = ic._get_prev_cell(current_row, CELL_BOUNDARIES)
+        prev_cell = jc._get_prev_cell(current_row, CELL_BOUNDARIES)
         self.assertEqual(prev_cell, 8)
 
     def test_get_prev_cell_cursor_end_of_cell(self):
         current_row = 14
-        prev_cell = ic._get_prev_cell(current_row, CELL_BOUNDARIES)
+        prev_cell = jc._get_prev_cell(current_row, CELL_BOUNDARIES)
         self.assertEqual(prev_cell, 8)
 
     def test_get_rows_with_tag(self):
@@ -113,7 +113,7 @@ class TestJuliaCell(unittest.TestCase):
             "## cell header",
             "print('cell content')",
         ]
-        rows = ic._get_rows_with_tag(buffer, "##")
+        rows = jc._get_rows_with_tag(buffer, "##")
         self.assertEqual(rows, [2])
 
     def test_get_rows_with_tag_multiple_rows(self):
@@ -128,7 +128,7 @@ class TestJuliaCell(unittest.TestCase):
             "    ## cell header 3",
             "    print(i)",
         ]
-        rows = ic._get_rows_with_tag(buffer, "##")
+        rows = jc._get_rows_with_tag(buffer, "##")
         self.assertEqual(rows, [1, 4, 8])
 
     def test_get_rows_with_marks(self):
@@ -136,7 +136,7 @@ class TestJuliaCell(unittest.TestCase):
             'a': (2, 1),
         }
         buffer = Buffer(marks=marks)
-        rows = ic._get_rows_with_marks(buffer, valid_marks='abcdefg')
+        rows = jc._get_rows_with_marks(buffer, valid_marks='abcdefg')
         self.assertEqual(rows, [2])
 
     def test_get_rows_with_marks_multiple_rows(self):
@@ -146,5 +146,5 @@ class TestJuliaCell(unittest.TestCase):
             'c': (8, 1),
         }
         buffer = Buffer(marks=marks)
-        rows = ic._get_rows_with_marks(buffer, valid_marks='abcdefg')
+        rows = jc._get_rows_with_marks(buffer, valid_marks='abcdefg')
         self.assertEqual(rows, [1, 4, 8])
